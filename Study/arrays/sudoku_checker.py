063d9c60728c0b71c210cdef24581f971a368bde
Ashun <ashun.kothari10@gmail.com>
2022-06-18 01:51:48 +0000

    Sudoku question

---
+++ Study/arrays/sudoku_checker.py
@@ -0,0 +1,32 @@
+"""
+Check whether a 9 x9 2D arcay representing a partially completed Sudoku is valid. Specifically,
+check that no row, column, or 3 x 3 2D subarray contains duplicates. A O-value in the 2D array
+indicates that entry is blank; every other entry is in [1,9].
+Hint: Directly test the constraints. Use an array to encode sets.
+"""
+import math
+def is_valid_sudoku(partial_assignment):
+    # Return True if subarray
+    # partial_assingnment[start_row:end_row][start_col:end:col] contains
+    # any duplicates in {1,2,... len(partial_assigment)}; otherewise return
+    # False
+    def has_duplicate(block):
+        block = list(filter(lambda x:x != 0, block))
+        return len(block) != len(set(block))
+    n = len(partial_assignment)
+    #  Check row and column constraints
+    if any(has_duplicate([partial_assignment[i][j] for j in range(n)])
+    or has_duplicate([partial_assignment[j][i] for j in range(n)])
+    for i in range(n)):
+        return False
+    
+    # check region constraints
+    region_size = int(math.sqrt(n))
+    return all(not has_duplicate([
+        partial_assignment[a][b]
+        for a in range(region_size * I, region_size*(I + J))
+        for b in range(region_size * J, region_size * (J * I))
+        ]) for I in range(region_size) for J in range(region_size))
+
+# Time complexity - O(n2)
+# Space complexit - O(n)

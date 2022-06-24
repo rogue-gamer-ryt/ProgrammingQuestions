3d381f8093141cefc15dd9e63b3a914ff916a13f
Ashun <ashun.kothari10@gmail.com>
2022-06-24 11:09:11 +0000

    buildings with sunset views

---
+++ Study/stack and queue/buildings_with_sunset_view.py
@@ -0,0 +1,20 @@
+"""
+You are given a series of buildings that have windows facing west. The buildings are in a straight
+line, and any building which is to the east of a building of equal or greater height cannot view the
+sunset.
+
+Design an algorithm that processes buildings in east-to-west order and returns the set of buildings
+which view the sunset. Each building is specified by its height.
+"""
+
+# Time Complexity: O(n)
+def examine_buildings_with_sunset(sequence):
+    building_with_height = collections.namedtuple('BuildingWithHeight', ('id', 'height'))
+    
+    candidates = []
+    for building_idx, building_height = enumerate(sequence):
+        while candidates and building_height >= candidates[-1].height:
+            candidates.pop()
+        candidates.append(building_with_height(building_idx, building_height))
+    
+    return [candidate.id for candidate in reversed(candidates)]

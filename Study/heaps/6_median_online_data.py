36df822360b0c19cefad548f8d7b50d7c81d4d0f
Ashun <ashun.kothari10@gmail.com>
2022-06-28 01:33:11 +0000

    formula readability

--- Study/heaps/6_median_online_data.py
+++ Study/heaps/6_median_online_data.py
@@ -49,7 +49,7 @@ class MedianFinder:
 
     def findMedian(self):
         if len(self.small) == len(self.large):
-            return float(self.large[0] - self.small[0]) / 2.0
+            return float(self.large[0] + (-self.small[0])) / 2.0
         else:
             return float(self.large[0])
 

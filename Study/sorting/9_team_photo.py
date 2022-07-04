07f0002462f0b28f7db142c319b36aaa84ff5e04
Ashun <ashun.kothari10@gmail.com>
2022-07-04 16:42:49 +0000

    team photo

---
+++ Study/sorting/9_team_photo.py
@@ -0,0 +1,15 @@
+"""
+Design an algorithm that takes as input two teams and the heights of the players in the teams and
+checks if it is possible to place players to take the photo subject to the placement constraint.
+"""
+
+class Team:
+    Player = collections.namedtuple('Player', ('height'))
+
+    def __init__(self, height):
+        self._players = [Team.Player(h) for h in height]
+
+    # Checks if A can be placed in front of B
+    @staticmethod
+    def valid_placement_exists(A, B):
+        return all(a < b for a, b in zip(sorted(A._players), sorted(B._players)))

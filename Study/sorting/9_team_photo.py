68d6c82df5422a719d7a8bbe76783f065475f193
Ashun <ashun.kothari10@gmail.com>
2022-07-04 16:43:02 +0000

    time complexity

--- Study/sorting/9_team_photo.py
+++ Study/sorting/9_team_photo.py
@@ -2,7 +2,7 @@
 Design an algorithm that takes as input two teams and the heights of the players in the teams and
 checks if it is possible to place players to take the photo subject to the placement constraint.
 """
-
+# TIme - O(nlogn)
 class Team:
     Player = collections.namedtuple('Player', ('height'))
 

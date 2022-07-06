8bd8fca771656170aa052b0ed44b183d9d80b27b
Ashun <ashun.kothari10@gmail.com>
2022-07-06 15:59:30 +0000

    lca in bst

---
+++ Study/binary_search_tree/4_compute_lca_in_bst.py
@@ -0,0 +1,18 @@
+"""
+Design an algorithm that takes as input a BST and two nodes, and retums the LCA of the two
+nodes.
+"""
+
+# Time - O(h)
+def find_LCA(tree, s, b):
+    while tree.data < s.data or tree.data > b.data:
+        # keep searching as tree is outside [s, b]
+        while tree.data < s.data:
+            tree = tree.right # LCA must be in tree's right child
+        while tree.data > b.data:
+            tree = tree.left # LCA must be in tree's left child
+        
+    # Now, s.data <= tree.data and tree.data <= b.data
+    return tree
+
+

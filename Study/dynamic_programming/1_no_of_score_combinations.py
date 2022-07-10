840ce00859a922cbcbb1f3c9fd21bbef9e2e1737
Ashun <ashun.kothari10@gmail.com>
2022-07-10 15:32:34 +0000

    no of score combination

---
+++ Study/dynamic_programming/1_no_of_score_combinations.py
@@ -0,0 +1,22 @@
+"""
+Write a program that takes a final score and scores for individual plays, and returns the number of
+combinations of plays that result in the final score.
+"""
+
+def num_combinations_for_final_score(final_score, individual_play_scores):
+    # one way to reach 0
+    num_combinations_for_score = [[1] + [0] * final_score for _ in individual_play_scores]
+
+    for i in range(len(individual_play_scores)):
+        for j in range(1, final_score + 1):
+            without_this_play = (num_combinations_for_score[i - 1][j] if i >= 1 else 0)
+
+            with_this_play = (
+                num_combinations_for_score[i][j - individual_play_scores[i]]
+                if j >= individual_play_scores[i] else 0
+            )
+            num_combinations_for_score[i][j] = (
+                without_this_play + with_this_play
+            )
+
+    return num_combinations_for_score[-1][-1]

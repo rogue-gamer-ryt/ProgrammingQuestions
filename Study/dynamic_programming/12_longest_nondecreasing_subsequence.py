fc0d997ad19413ad1e016834340474d686e60835
Ashun <ashun.kothari10@gmail.com>
2022-07-16 13:07:20 +0000

    Longest nondecreasing subsequence

---
+++ Study/dynamic_programming/12_longest_nondecreasing_subsequence.py
@@ -0,0 +1,18 @@
+"""
+Write a Program that takes as input an array of numbers and retums the length of a longest
+nondecreasing subsequence in the array.
+"""
+
+# Time O(N^2)
+def longest_nondecreasing_subsequence_length(A):
+# max_length[i] holds the length of the longest nondecreasing subsequeunce
+# of A[:i + 1]
+max_length = [1] * len(A)
+for i in range(1, len(A)):
+    max_length[i] = max(
+            1 + max([
+                max_length[j] for j in range(i) if A[i] >= A[j]
+                ], default = 0),
+            max_length[i]
+        )
+return max(max_length)

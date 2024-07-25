c4ff28a14b5c723193be093828510d931e673189
Ashun Kothari <ashun.kothari10@gmail.com>
2024-07-25 13:41:53 -0400

    lca

--- Study/binary_search_tree/4_compute_lca_in_bst.py
+++ Study/binary_search_tree/4_compute_lca_in_bst.py
@@ -3,16 +3,15 @@ Design an algorithm that takes as input a BST and two nodes, and retums the LCA 
 nodes.
 """
 
+
 # Time - O(h)
 def find_LCA(tree, s, b):
     while tree.data < s.data or tree.data > b.data:
         # keep searching as tree is outside [s, b]
         while tree.data < s.data:
-            tree = tree.right # LCA must be in tree's right child
+            tree = tree.right  # LCA must be in tree's right child
         while tree.data > b.data:
-            tree = tree.left # LCA must be in tree's left child
-        
+            tree = tree.left  # LCA must be in tree's left child
+
     # Now, s.data <= tree.data and tree.data <= b.data
     return tree
-
-
--- Study/binary_tree/lowest_common_ancestor.py
+++ Study/binary_tree/lowest_common_ancestor.py
@@ -6,23 +6,31 @@ of both nodes.
 Design an algorithm for computing the LCA of two nodes in a binary tree in which nodes do not
 have a parent field.
 """
+import collections
+
+
 # Traverse the tree and return the count of nodes found which matches the nodes
 # to search
 # If reached end, return 0 and None
 
 # Time Complexity - O(n)
 # Space Complexity - O(h)
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 def lca(tree, node0, node1):
     Status = collections.namedtuple('Status', ('num_of_nodes_found', 'ancestor'))
 
-    def lca_helper(tree, node0, node1):
+    def lca_helper(tree: TreeNode, node0: TreeNode, node1: TreeNode):
         if not tree:
             return Status(0, None)
         
-        left_result = lca_helper(tree.left, node0, node1):
+        left_result = lca_helper(tree.left, node0, node1)
         if left_result.num_of_nodes_found == 2:
             return left_result
-        right_result = lca_helper(tree.left, node0, node1):
+        right_result = lca_helper(tree.left, node0, node1)
         if right_result.num_of_nodes_found == 2:
             return right_result
                 

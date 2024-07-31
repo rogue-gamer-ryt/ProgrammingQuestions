3d534969afd4ff05e94577208feee625527705a0
Ashun Kothari <ashun.kothari10@gmail.com>
2024-07-31 16:07:54 -0400

    Search words data structure

---
+++ Study/trie/search_words_in_datastructure.py
@@ -0,0 +1,74 @@
+"""
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object. void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise.
+word may contain dots '.' where dots can be matched with any letter.
+
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+
+Link: https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+"""
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfPath = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfPath = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(i, root):
+            curr = root
+
+            for j in range(i, len(word)):
+                c = word[j]
+                if c == ".":
+                    for child in curr.children.values():
+                        if dfs( j+ 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.endOfPath
+
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)

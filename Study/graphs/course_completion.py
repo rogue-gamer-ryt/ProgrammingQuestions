97711181705618dbefb2146c1c2ca31933de121a
Ashun Kothari <ashun.kothari10@gmail.com>
2024-08-09 10:08:39 -0400

    Course Schedule

---
+++ Study/graphs/course_completion.py
@@ -0,0 +1,46 @@
+"""
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array
+prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take
+course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take.
+To take course 1 you should have finished course 0. So it is possible.
+
+Link: https://leetcode.com/problems/course-schedule/
+"""
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visited = set()
+
+        # DFS to check if the course can be completed
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True

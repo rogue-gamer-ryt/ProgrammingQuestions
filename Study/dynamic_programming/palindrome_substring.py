a58d74319319f2f40ff981ff68865c2e81f05e56
Ashun Kothari <ashun.kothari10@gmail.com>
2024-09-13 15:36:38 -0400

    Palindromic Substrings

---
+++ Study/dynamic_programming/palindrome_substring.py
@@ -0,0 +1,33 @@
+"""
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Link: https://leetcode.com/problems/palindromic-substrings/
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            # Odd
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            # Even
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        return count

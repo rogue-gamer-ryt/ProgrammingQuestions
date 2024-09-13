881d5990afd5a88cdf8b8b5bd354bf7b285295c1
Ashun Kothari <ashun.kothari10@gmail.com>
2024-09-13 12:36:54 -0400

    Longest Palindrome

---
+++ Study/dynamic_programming/longest_palindrome.py
@@ -0,0 +1,33 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Link: https://leetcode.com/problems/longest-palindromic-substring/
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+        for i in range(len(s)):
+            # Odd Length Palindrome
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            # Even Length Palindrome
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res

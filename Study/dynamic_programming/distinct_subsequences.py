8d688bead496a0baf8b19c4259b7323712dfd2cb
Ashun Kothari <ashun.kothari10@gmail.com>
2024-09-20 14:05:40 -0400

    Distinct Subsequence

---
+++ Study/dynamic_programming/distinct_subsequences.py
@@ -0,0 +1,31 @@
+"""
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+Example 1:
+
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from s.
+rabbbit
+rabbbit
+rabbbit
+
+Link: https://leetcode.com/problems/distinct-subsequences/
+"""
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
+        for i in range(len(s) + 1):
+            dp[i][len(t)] = 1
+
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(len(t) - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j] + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = dp[i + 1][j]
+
+        return dp[0][0]

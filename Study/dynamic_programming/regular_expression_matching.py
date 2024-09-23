cc055c00b6e23296bda9d31e634d88493120dfa0
Ashun Kothari <ashun.kothari10@gmail.com>
2024-09-23 16:19:43 -0400

    Regular Expression Matching

---
+++ Study/dynamic_programming/regular_expression_matching.py
@@ -0,0 +1,41 @@
+"""
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Link: https://leetcode.com/problems/regular-expression-matching/
+"""
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        cache = {}
+
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                # if a * is there, we can either use it or not use it
+                cache[(i, j)] = ((match and dfs(i + 1, j)) or dfs(i, j + 2))
+                return cache[(i, j)]
+            if match:
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            cache[(i, j)] = False
+            return False
+
+        return dfs(0, 0)

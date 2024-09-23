6afc12c61a6644f81fed6cb9f3b87fe9ec9a12b3
Ashun Kothari <ashun.kothari10@gmail.com>
2024-09-23 19:21:18 -0400

    Maximum Subarray

---
+++ Study/greedy/maximum_subarray.py
@@ -0,0 +1,26 @@
+"""
+Given an integer array nums, find the
+subarray
+ with the largest sum, and return its sum.
+
+
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Link: https://leetcode.com/problems/maximum-subarray/
+"""
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        total = 0
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res

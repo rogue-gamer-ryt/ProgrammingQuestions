a93cad70718b8137111bb332ecb37c2888d724de
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-11 11:13:38 -0400

    Spiral Matrix

--- Study/arrays/spiral_2D_array.py
+++ Study/arrays/spiral_2D_array.py
@@ -98,3 +98,41 @@ assert generate_spiral_matrix_from_order(3) == [
     [8, 9, 4],
     [7, 6, 5]
 ]
+
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Link: https://leetcode.com/problems/spiral-matrix/
+"""
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            # Top Left -> Top Right
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+            # Top Right -> Bottom Right
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+            # Bottom Right -> Bottom Left
+            for i in reversed(range(left, right)):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+            # Bottom Left -> Top Left
+            for i in reversed(range(top, bottom)):
+                res.append(matrix[i][left])
+            left += 1
+        return res

49f12a5a71e9cdbea81f201b1b5211439f12294a
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-10 22:22:46 -0400

    Time based KV store

---
+++ Study/searching/binary_search/time_based_key_value_store.py
@@ -0,0 +1,44 @@
+"""
+Design a time-based key-value data structure that can store multiple values for the same key at different time
+stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with
+timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest
+timestamp_prev. If there are no values, it returns "".
+
+Link: https://leetcode.com/problems/time-based-key-value-store/
+"""
+
+
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.store.get(key, [])
+
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+
+    # Your TimeMap object will be instantiated and called as such:
+    # obj = TimeMap()
+    # obj.set(key,value,timestamp)
+    # param_2 = obj.get(key,timestamp)

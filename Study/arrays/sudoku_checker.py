593027ba3a43f3a046bfcec2bebe8b4dee83a5e4
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-11 10:08:55 -0400

    Valid Sudoku

--- Study/arrays/sudoku_checker.py
+++ Study/arrays/sudoku_checker.py
@@ -31,3 +31,26 @@ def is_valid_sudoku(partial_assignment):
 
 # Time complexity - O(n2)
 # Space complexit - O(n)
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                if (
+                        board[r][c] in rows[r] or
+                        board[r][c] in cols[c] or
+                        board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True

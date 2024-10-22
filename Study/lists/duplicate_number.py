e421c6b44a8d64b45575cb7df105b39ec9426171
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-22 19:42:09 -0400

    Duplicate Number

---
+++ Study/lists/duplicate_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+        fast = nums[0]
+        while fast !=  slow:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow

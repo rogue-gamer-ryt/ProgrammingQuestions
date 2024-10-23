a0ed1102c52d38b4e978668046f812f4a51b93c7
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-23 14:46:13 -0400

    Group Anagrams

---
+++ Study/arrays/group_anagrams.py
@@ -0,0 +1,28 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Link: https://leetcode.com/problems/group-anagrams/
+"""
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            freq = [0] * 26
+            for c in s:
+                freq[ord(c) - ord("a")] += 1
+            ans[tuple(freq)].append(s)
+
+        return list(ans.values())

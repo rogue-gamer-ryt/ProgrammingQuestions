6bd1b9151a6315fe5258ee60b7ec9bde25966385
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-23 15:24:26 -0400

    Reverse Linked List

--- Study/lists/remove_kth_element_from_last.py
+++ Study/lists/remove_kth_element_from_last.py
@@ -32,3 +32,26 @@ def remove_kth_last(L, k):
     second.next = second.next.next
     return dummy_head.next
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
---
+++ Study/lists/reverse_linked_list.py
@@ -0,0 +1,27 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Link: https://leetcode.com/problems/reverse-linked-list/
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev

8ee49c4eccb9e816d4f072d6aec37286ba4e285b
Ashun Kothari <ashun.kothari10@gmail.com>
2024-10-30 15:44:08 -0400

    Container with most water

--- Study/strings/palindrome.py
+++ Study/strings/palindrome.py
@@ -5,6 +5,15 @@ def is_palindromic(s):
     # s[~i] is s[-(i + 1)]
     return all(s[i] == s[~i] for i in range(len(s) // 2))
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+
+        for c in s:
+            if c.isalpha() or c.isdigit():
+                new_s += c.lower()
+        return (new_s == new_s[::-1])
+
 assert is_palindromic("aba") is True
 assert is_palindromic("abx") is False
 assert is_palindromic("ababa") is True

bb56b83d48aa1caa94df866f7f8240bfb57dded0
Ashun Kothari <ashun.kothari10@gmail.com>
2024-11-04 16:33:20 -0500

    Three Sum

--- Study/greedy/4_three_sum_problem.py
+++ Study/greedy/4_three_sum_problem.py
@@ -17,4 +17,32 @@ def has_two_sum(A, t):
 def has_three_sum(A, t):
     A.sort()
     return any(has_two_sum(A, t - a) for a in A)
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                curr_sum = a + nums[l] + nums[r]
+                if curr_sum > 0:
+                    r -= 1
+                elif curr_sum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
         

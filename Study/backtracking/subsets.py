fa6f4f0f78739bfd17f45ce2df27601b2bd3509d
Ashun Kothari <ashun.kothari10@gmail.com>
2024-11-08 14:39:57 -0500

    Subsets

---
+++ Study/backtracking/subsets.py
@@ -0,0 +1,36 @@
+"""
+Given an integer array nums of unique elements, return all possible
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Link: https://leetcode.com/problems/subsets/
+"""
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+
+        return res
